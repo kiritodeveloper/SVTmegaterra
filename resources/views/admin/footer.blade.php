e5a742f9a9c5e8b1dd01c90e00f95234ceb1453a
kiritodeveloper <macsoftins@gmail.com>
2018-12-10 20:30:55 -0400

    Story 182: Fix benchmarks for speed

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,11 @@
+    <div class="footer-copyright-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="footer-copy-right">
+                        <p>Copyright © 2018. todos los derechos reservados   <a href="https://macsoftins.com">m@c-softins</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,445 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard Two | Notika - Notika Admin Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="./adminlte/img/favicon.ico">
+    <!-- Google Fonts
+		============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/bootstrap.min.css">
+    <!-- font awesome CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/font-awesome.min.css">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/owl.carousel.css">
+    <link rel="stylesheet" href="./adminlte/css/owl.theme.css">
+    <link rel="stylesheet" href="./adminlte/css/owl.transitions.css">
+    <!-- meanmenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/meanmenu/meanmenu.min.css">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/animate.css">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/normalize.css">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <!-- jvectormap CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/jvectormap/jquery-jvectormap-2.0.3.css">
+    <!-- Notika icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/notika-custom-icon.css">
+    <!-- wave CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/wave/waves.min.css">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/main.css">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/style.css">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="./adminlte/css/responsive.css">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="./adminlte/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<style type="text/css">
+    
+body {
+  background: #F1F3FA;
+}
+
+/* Profile container */
+.profile {
+  margin: 20px 0;
+}
+
+/* Profile sidebar */
+.profile-sidebar {
+  padding: 20px 0 10px 0;
+  background: #fff;
+}
+
+.profile-userpic img {
+  float: none;
+  margin: 0 auto;
+  width: 50%;
+  height: 50%;
+  -webkit-border-radius: 50% !important;
+  -moz-border-radius: 50% !important;
+  border-radius: 50% !important;
+}
+
+.profile-usertitle {
+  text-align: center;
+  margin-top: 20px;
+}
+
+.profile-usertitle-name {
+  color: #5a7391;
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 7px;
+}
+
+.profile-usertitle-job {
+  text-transform: uppercase;
+  color: #5b9bd1;
+  font-size: 12px;
+  font-weight: 600;
+  margin-bottom: 15px;
+}
+
+.profile-userbuttons {
+  text-align: center;
+  margin-top: 10px;
+}
+
+.profile-userbuttons .btn {
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: 600;
+  padding: 6px 15px;
+  margin-right: 5px;
+}
+
+.profile-userbuttons .btn:last-child {
+  margin-right: 0px;
+}
+    
+.profile-usermenu {
+  margin-top: 30px;
+}
+
+.profile-usermenu ul li {
+  border-bottom: 1px solid #f0f4f7;
+}
+
+.profile-usermenu ul li:last-child {
+  border-bottom: none;
+}
+
+.profile-usermenu ul li a {
+  color: #93a3b5;
+  font-size: 14px;
+  font-weight: 400;
+}
+
+.profile-usermenu ul li a i {
+  margin-right: 8px;
+  font-size: 14px;
+}
+
+.profile-usermenu ul li a:hover {
+  background-color: #fafcfd;
+  color: #5b9bd1;
+}
+
+.profile-usermenu ul li.active {
+  border-bottom: none;
+}
+
+.profile-usermenu ul li.active a {
+  color: #5b9bd1;
+  background-color: #f6f9fb;
+  border-left: 2px solid #5b9bd1;
+  margin-left: -2px;
+}
+
+/* Profile Content */
+.profile-content {
+  padding: 20px;
+  background: #fff;
+  min-height: 460px;
+}
+</style>
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- Start Header Top Area -->
+    <div class="header-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo-area">
+                        <a href="#"><img src="./adminlte/img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                    <div class="header-top-menu">
+                        <ul class="nav navbar-nav notika-top-nav">
+                            <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span><i class="notika-icon notika-support"></i></span></a>
+                                <div role="menu" class="dropdown-menu message-dd chat-dd animated zoomIn">
+                                    <div class="container">
+                                    <div class="row profile">
+                                    <div class="col-md-3">
+                                        <div class="profile-sidebar">
+                                            <!-- SIDEBAR USERPIC -->
+                                            <div class="profile-userpic">
+                                                <img src="./adminlte/img/post/suus.png" class="img-responsive" alt="">
+                                            </div>
+                                            <div class="profile-usertitle">
+                                                <div class="profile-usertitle-name">
+                                                    administrador
+                                                </div>
+                                                <div class="profile-usertitle-job">
+                                                    descripcion
+                                                </div>
+                                            </div>
+                                            <div class="profile-userbuttons">
+                                                <button type="button" class="btn btn-success btn-sm">salir</button>
+                                                <button type="button" class="btn btn-danger btn-sm">editar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="main-menu-area mg-tb-40"> 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
+                        <li class="active"><a data-toggle="tab" href="#Home"><i class="notika-icon notika-house"></i> Home</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> Email</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Interface"><i class="notika-icon notika-edit"></i> Interface</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Charts"><i class="notika-icon notika-bar-chart"></i> Charts</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Tables"><i class="notika-icon notika-windows"></i> Tables</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Forms"><i class="notika-icon notika-form"></i> Forms</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Appviews"><i class="notika-icon notika-app"></i> App views</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Page"><i class="notika-icon notika-support"></i> Pages</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content custom-menu-content">
+                        <div id="Home" class="tab-pane in active notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="index.html">Dashboard One</a>
+                                </li>
+                                <li><a href="index-2.html">Dashboard Two</a>
+                                </li>
+                                <li><a href="index-3.html">Dashboard Three</a>
+                                </li>
+                                <li><a href="index-4.html">Dashboard Four</a>
+                                </li>
+                                <li><a href="analytics.html">Analytics</a>
+                                </li>
+                                <li><a href="widgets.html">Widgets</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="mailbox" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="inbox.html">Inbox</a>
+                                </li>
+                                <li><a href="view-email.html">View Email</a>
+                                </li>
+                                <li><a href="compose-email.html">Compose Email</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Interface" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="animations.html">Animations</a>
+                                </li>
+                                <li><a href="google-map.html">Google Map</a>
+                                </li>
+                                <li><a href="data-map.html">Data Maps</a>
+                                </li>
+                                <li><a href="code-editor.html">Code Editor</a>
+                                </li>
+                                <li><a href="image-cropper.html">Images Cropper</a>
+                                </li>
+                                <li><a href="wizard.html">Wizard</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Charts" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="flot-charts.html">Flot Charts</a>
+                                </li>
+                                <li><a href="bar-charts.html">Bar Charts</a>
+                                </li>
+                                <li><a href="line-charts.html">Line Charts</a>
+                                </li>
+                                <li><a href="area-charts.html">Area Charts</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Tables" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="normal-table.html">Normal Table</a>
+                                </li>
+                                <li><a href="data-table.html">Data Table</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Forms" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="form-elements.html">Form Elements</a>
+                                </li>
+                                <li><a href="form-components.html">Form Components</a>
+                                </li>
+                                <li><a href="form-examples.html">Form Examples</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Appviews" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="notification.html">Notifications</a>
+                                </li>
+                                <li><a href="alert.html">Alerts</a>
+                                </li>
+                                <li><a href="modals.html">Modals</a>
+                                </li>
+                                <li><a href="buttons.html">Buttons</a>
+                                </li>
+                                <li><a href="tabs.html">Tabs</a>
+                                </li>
+                                <li><a href="accordion.html">Accordion</a>
+                                </li>
+                                <li><a href="dialog.html">Dialogs</a>
+                                </li>
+                                <li><a href="popovers.html">Popovers</a>
+                                </li>
+                                <li><a href="tooltips.html">Tooltips</a>
+                                </li>
+                                <li><a href="dropdown.html">Dropdowns</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="Page" class="tab-pane notika-tab-menu-bg animated flipInX">
+                            <ul class="notika-main-menu-dropdown">
+                                <li><a href="contact.html">Contact</a>
+                                </li>
+                                <li><a href="invoice.html">Invoice</a>
+                                </li>
+                                <li><a href="typography.html">Typography</a>
+                                </li>
+                                <li><a href="color.html">Color</a>
+                                </li>
+                                <li><a href="login-register.html">Login Register</a>
+                                </li>
+                                <li><a href="404.html">404 Page</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>  
+</body>
+    </section class="mobile-menu-area">
+        @yield('content')
+    <section>
+        
+    </section>
+
+    <footer class="row">
+        @include('admin.footer')
+    </footer>
+    <!-- End Footer area-->
+    <!-- jquery
+		============================================ -->
+    <script src="./adminlte/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="./adminlte/js/bootstrap.min.js"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="./adminlte/js/wow.min.js"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="./adminlte/js/jquery-price-slider.js"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="./adminlte/js/owl.carousel.min.js"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="./adminlte/js/jquery.scrollUp.min.js"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="./adminlte/js/meanmenu/jquery.meanmenu.js"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="./adminlte/js/counterup/jquery.counterup.min.js"></script>
+    <script src="./adminlte/js/counterup/waypoints.min.js"></script>
+    <script src="./adminlte/js/counterup/counterup-active.js"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="./adminlte/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+    <!-- jvectormap JS
+		============================================ -->
+    <script src="./adminlte/js/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="./adminlte/js/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="./adminlte/js/jvectormap/jvectormap-active.js"></script>
+    <!-- sparkline JS
+		============================================ -->
+    <script src="./adminlte/js/sparkline/jquery.sparkline.min.js"></script>
+    <script src="./adminlte/js/sparkline/sparkline-active.js"></script>
+    <!-- flot JS
+		============================================ -->
+    <script src="./adminlte/js/flot/jquery.flot.js"></script>
+    <script src="./adminlte/js/flot/jquery.flot.resize.js"></script>
+    <script src="./adminlte/js/flot/curvedLines.js"></script>
+    <script src="./adminlte/js/flot/flot-active.js"></script>
+    <!-- knob JS
+		============================================ -->
+    <script src="./adminlte/js/knob/jquery.knob.js"></script>
+    <script src="./adminlte/js/knob/jquery.appear.js"></script>
+    <script src="./adminlte/js/knob/knob-active.js"></script>
+    <!--  wave JS
+		============================================ -->
+    <script src="./adminlte/js/wave/waves.min.js"></script>
+    <script src="./adminlte/js/wave/wave-active.js"></script>
+    <!--  Chat JS
+		============================================ -->
+	<script src="./adminlte/js/chat/moment.min.js"></script>
+    <script src="./adminlte/js/chat/jquery.chat.js"></script>
+    <!--  todo JS
+		============================================ -->
+    <script src="./adminlte/js/todo/jquery.todo.js"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="./adminlte/js/plugins.js"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="./adminlte/js/main.js"></script>
+	<!-- tawk chat JS
+		============================================ -->
+    
+</body>
+
+</html>
