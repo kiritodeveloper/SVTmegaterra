78a663ee1b2519b8725856550976a67fee1aa872
kiritodeveloper <macsoftins@gmail.com>
2019-01-05 23:40:49 -0400

    mejorando interface cliente

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -42,9 +42,8 @@
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
                                     <button type="submit" class="btn btn-round btn-danger"><span class="glyphicon glyphicon-trash"></span></button>
-                                </form>
-                           <a href="#" class="btn btn-round btn-warning"><span class="glyphicon glyphicon-eye-open"></span></a>
-                           <a href="{{ route('clientes.edit', $cliente) }}" class="btn btn-round btn-success"><span class="glyphicon glyphicon-edit"></span></a>
+                                    <a href="{{ route('clientes.edit', $cliente) }}" class="btn btn-round btn-success"><span class="glyphicon glyphicon-edit"></span></a>
+                                </form>                           
                     </div>
                 </div>
     
