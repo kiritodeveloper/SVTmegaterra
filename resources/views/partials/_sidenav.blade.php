f9ea7ea1bff2dcdac525b0d67b6465d8501bcc8c
kiritodeveloper <macsoftins@gmail.com>
2019-02-26 18:59:51 -0400

    portada

--- resources/views/partials/_sidenav.blade.php
+++ resources/views/partials/_sidenav.blade.php
@@ -26,27 +26,24 @@
                     </li>-->
                     <li><a><i class="fa fa fa-group"></i> Administracion <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a href="{{route('clientes.create')}}">Registro De Clientes</a></li>
-                            <li><a href="{{ route('users.create') }}"> Registro De Usuarios</a></li>
+                            <li><a href="{{route('clientes.create')}}">Clientes</a></li>
+                            <li><a href="{{ route('users.create') }}"> Usuarios</a></li>
+                            <li><a href="{{ route('proyecto.create') }}">Proyectos</a></li>
+                            <li><a href="{{ route('tipo.create') }}">Tipos de Terreno</a></li>
+                            <li><a href="{{ route('map.index') }}">Terrenos</a></li>
+                            <li><a href="#">Planes</a></li>
                             <li><a href="{{ route('users.index') }}"> Listar Usuarios</a></li>
                             <li><a href="{{ route('clientes.index') }}">Lista de clientes</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa fa-home"></i>Terrenos <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="{{ route('proyecto.create') }}">Registro De Proyectos</a></li>
-                            <li><a href="{{ route('tipo.create') }}">Registro De Tipos de Terreno</a></li>
-                            <li><a href="{{ route('map.index') }}">registro de terrenos</a></li>
-                            <li><a href="#">Resgistro De Planes</a></li>
                             <li><a href="{{ route('proyecto.index') }}">Lista De Proyectos</a></li>
                             <li><a href="{{ route('tipo.index') }}">Lista De Tipos de Terreno</a></li>
-                            <li><a href="">Lista De Terrenos</a></li>
-                            <li><a href="{{route('tipo.index')}}">Lista De Planes</a></li>
                         </ul>
                     </li>
+
                     <li><a><i class="fa fa-credit-card"></i>Venta De Terrenos <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
+
+                            <li><a href="">Lista De Terrenos</a></li>
+                            <li><a href="{{route('tipo.index')}}">Lista De Planes</a></li>
                             <li><a href="#">Venta De Terrenos</a></li>
                             <li><a href="#">lista De Ventas</a></li>
                             <li><a href="#">Reportes Del Dia</a></li>
